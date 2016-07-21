f3759c936905a98d34323a712f89c70c5c9b8f3b
zeus <1203508644@qq.com>
2016-07-21 23:19:23 +0800

    2016年7月21日

---
+++ zero-service-login/src/main/java/com/zzy/gate/login/ServiceHandler.java
@@ -0,0 +1,55 @@
+package com.zzy.gate.login;
+
+import java.util.Objects;
+
+import org.apache.log4j.Logger;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.springframework.util.StringUtils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPObject;
+import com.zzy.common.util.EasyString;
+
+/**
+* @author Zeus
+ * @version 1.1
+ * @createTime：2016年7月20日 @decript: 逻辑处理类,客户端请求进入里面
+ */
+public class ServiceHandler extends IoHandlerAdapter {
+	private static Logger log = Logger.getLogger(ServiceHandler.class);
+
+	// 当一个客户端连接进入时
+	@Override
+	public void sessionOpened(IoSession session) throws Exception {
+	
+	}
+
+	/**
+	 * 捕捉异常
+	 */
+	@Override
+	public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+		System.out.println("异常"+cause.getMessage());
+		log.error("错误session:"+session.getId()+"---->"+cause.getMessage());
+	}
+
+	/**
+	 * 当客户端发送消息到达时
+	 */
+	@Override
+	public void messageReceived(IoSession session, Object message) throws Exception {
+		// 收到的信息字符串
+		if(Objects.isNull(message) || "null".equals(message)){
+			throw new NullPointerException("message不能为null");
+		}
+		JSONObject json = JSONObject.parseObject((String) message);
+	}
+
+
+	// 当一个客户端连接关闭时
+	@Override
+	public void sessionClosed(IoSession session) throws Exception {
+		
+	}
+}
---
+++ zero-service-login/src/test/java/com/zzy/service/gate/Test.java
@@ -0,0 +1,18 @@
+package com.zzy.service.gate;
+
+/**
+* @author Zeus
+* @version 1.1
+* @createTime：2016年7月19日 
+* @decript:
+*/
+public class Test extends A {
+   public static void main(String[] args) {
+}
+
+}
+class A{
+	public void getMessage(){
+		System.out.println("A");
+	}
+}
